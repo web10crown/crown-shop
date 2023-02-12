573182a0e1d5c13b4fb3e1fcab3f1c239da39e34
web10crown <99588906+web10crown@users.noreply.github.com>
2023-02-12 23:58:22 +0530

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://backend-crown-shop.onrender.com/api/";
+const BASE_URL = "https://crown-shop-backend.vercel.app/api/";
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
